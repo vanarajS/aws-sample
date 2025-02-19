196097cc229a92c04f2c2ed8cfdf418dbc8859e2
vanaraj <vanaraj@book.com>
2025-02-19 21:47:46 +0530

    c

--- EKS_terraform/3-eks.tf
+++ EKS_terraform/3-eks.tf
@@ -1,27 +1,27 @@
-# resource "aws_iam_role" "eks_role" {
-#     name = "eks-role2"
-#     assume_role_policy = jsonencode({
-#         Version = "2012-10-17"
-#         Statement = [
-#             {
-#                 Action = [
-#                     "sts:AssumeRole",
-#                 ]
-#                 Effect = "Allow"
-#                 Principal = {
-#                     Service = "eks.amazonaws.com"
-#                 }
-#             },
-#         ]
-#     })
+resource "aws_iam_role" "eks_role" {
+    name = "eks-role2"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "sts:AssumeRole",
+                ]
+                Effect = "Allow"
+                Principal = {
+                    Service = "eks.amazonaws.com"
+                }
+            },
+        ]
+    })
 
-#     }
+    }
 
-# resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-#     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#     role = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role = aws_iam_role.eks_role.name
   
-# }
+}
 
 resource "aws_eks_cluster" "eks_cluster" {
     name = var.cluster_name
