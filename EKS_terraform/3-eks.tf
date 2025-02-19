819fa2bf71949ea2fbe4f20b6b1a7767688c7686
vanaraj <vanaraj@book.com>
2025-02-19 21:42:28 +0530

    g

--- EKS_terraform/3-eks.tf
+++ EKS_terraform/3-eks.tf
@@ -1,31 +1,31 @@
-resource "aws_iam_role" "eks_role" {
-    name = "eks-role2"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = [
-                    "sts:AssumeRole",
-                ]
-                Effect = "Allow"
-                Principal = {
-                    Service = "eks.amazonaws.com"
-                }
-            },
-        ]
-    })
+# resource "aws_iam_role" "eks_role" {
+#     name = "eks-role2"
+#     assume_role_policy = jsonencode({
+#         Version = "2012-10-17"
+#         Statement = [
+#             {
+#                 Action = [
+#                     "sts:AssumeRole",
+#                 ]
+#                 Effect = "Allow"
+#                 Principal = {
+#                     Service = "eks.amazonaws.com"
+#                 }
+#             },
+#         ]
+#     })
 
-    }
+#     }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role = aws_iam_role.eks_role.name
+# resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+#     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#     role = aws_iam_role.eks_role.name
   
-}
+# }
 
 resource "aws_eks_cluster" "eks_cluster" {
     name = var.cluster_name
-    role_arn = aws_iam_role.eks_role.arn
+    role_arn = "arn:aws:iam::637423644535:role/EKS-role"
     vpc_config {
       
       endpoint_private_access = true
