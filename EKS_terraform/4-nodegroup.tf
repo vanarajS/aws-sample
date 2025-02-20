e8c0a63bbc6a3c15247756aa56b75dbdbf116a25
vanaraj <vanaraj@book.com>
2025-02-20 16:13:36 +0530

    added node-group

---
+++ EKS_terraform/4-nodegroup.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_node_group" "ng1" {
+    cluster_name = var.cluster_name
+    node_role_arn = "arn:aws:iam::590183971315:role/eksCluserRole"
+    node_group_name = "ng1"
+    subnet_ids = [
+        aws_subnet.pri-sn1.id,
+        aws_subnet.pri-sn2.id
+    ]
+    scaling_config {
+      desired_size = 3
+      max_size = 4
+      min_size = 1
+    }
+    update_config {
+      max_unavailable = 1
+    }
+}
